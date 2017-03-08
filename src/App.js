1e8a223b4ec8bbef27857c82448cd8de65dc4d63
Nai Saevang <nai.saevang@seedspark.com>
2017-03-08 16:44:41 -0500

    Layout refactor

--- src/App.js
+++ src/App.js
@@ -1,108 +1,110 @@
 import React, { Component } from 'react';
-import Textfield from './components/Textfield';
-import Checkbox from './components/Checkbox';
-import Drawer from './components/Drawer';
+import { MDCTemporaryDrawer } from '@material/drawer/dist/mdc.drawer';
 
 class App extends Component {
   state = {
-    drawerOpen: false
+    toolbarElevated: false
   };
 
-  toggleDrawer = () => {
-    this.setState({ drawerOpen: !this.state.drawerOpen });
+  mdcDrawer = null;
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.onScroll);
+    this.mdcDrawer = new MDCTemporaryDrawer(this._drawer);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.onScroll);
+    this.mdcDrawer.destroy();
+  }
+
+  onScroll = () => {
+    if (window.pageYOffset > 50) {
+      this.setState({ toolbarElevated: true });
+    } else {
+      this.setState({ toolbarElevated: false });
+    }
+  };
+
+  openDrawer = () => {
+    this.mdcDrawer.open = true;
   };
 
   render() {
+    const { toolbarElevated } = this.state;
     return (
       <div className="mdc-typography">
-        <nav className="mdc-toolbar mdc-toolbar--fixed">
+        <nav
+          className={
+            `mdc-toolbar mdc-toolbar--fixed ${!toolbarElevated ? 'mdc-elevation--z0' : ''}`
+          }
+        >
           <section
             className="mdc-toolbar__section mdc-toolbar__section--align-start"
           >
-            <a className="material-icons" onClick={this.toggleDrawer}>menu</a>
+            <a className="material-icons" onClick={this.openDrawer}>menu</a>
             <span className="mdc-toolbar__title">Title</span>
           </section>
           <section
             className="mdc-toolbar__section mdc-toolbar__section--align-end"
           >
-            <a
-              href
-              className="material-icons mdc-theme--text-primary-on-primary"
-            >
+            <a className="material-icons mdc-theme--text-primary-on-primary">
               search
             </a>
-            <a
-              href
-              className="material-icons mdc-theme--text-primary-on-primary"
-            >
+            <a className="material-icons mdc-theme--text-primary-on-primary">
               favorite
             </a>
-            <a
-              href
-              className="material-icons mdc-theme--text-primary-on-primary"
-            >
+            <a className="material-icons mdc-theme--text-primary-on-primary">
               more_vert
             </a>
           </section>
         </nav>
-        <Drawer open={this.state.drawerOpen} />
+        <aside className="mdc-temporary-drawer" ref={el => this._drawer = el}>
+          <nav className="mdc-temporary-drawer__drawer">
+            <nav className="mdc-temporary-drawer__content mdc-list-group">
+              <div className="mdc-list">
+                <a className="mdc-list-item" href="#">
+                  <i
+                    className="material-icons mdc-list-item__start-detail"
+                    aria-hidden="true"
+                  >
+                    email
+                  </i>
+                  All Mail
+                </a>
+                <a className="mdc-list-item" href="#">
+                  <i
+                    className="material-icons mdc-list-item__start-detail"
+                    aria-hidden="true"
+                  >
+                    delete
+                  </i>
+                  Trash
+                </a>
+                <a className="mdc-list-item" href="#">
+                  <i
+                    className="material-icons mdc-list-item__start-detail"
+                    aria-hidden="true"
+                  >
+                    report
+                  </i>
+                  Spam
+                </a>
+              </div>
+            </nav>
+          </nav>
+        </aside>
         <main className="mdc-toolbar-fixed-adjust">
           <div className="mdc-layout-grid">
             <div
-              className="mdc-layout-grid__cell mdc-layout-grid__cell--span-6 text-right"
-            >
-              <FormDemo />
-            </div>
-            <div
               className="mdc-layout-grid__cell mdc-layout-grid__cell--span-6"
             >
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, aut minima commodi temporibus iure neque quod quam, possimus reiciendis iusto alias sed culpa facilis, veniam unde et eum illum dolorem.
-              </p>
+              <div className="cell-placeholder">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium ab facere ullam enim deserunt sit ipsam quis. Nulla nostrum, ipsa in corporis minima! Dolorem quae quos, ea libero quasi consequuntur!
+              </div>
             </div>
           </div>
         </main>
-      </div>
-    );
-  }
-}
-
-class FormDemo extends Component {
-  state = {
-    feels: '',
-    showField: false
-  };
-
-  onChangeFeels = event => {
-    this.setState({ feels: event.target.value });
-  };
-
-  toggleField = () => {
-    this.setState({ showField: !this.state.showField });
-  };
-
-  render() {
-    return (
-      <div>
-        <div className="mdc-typography--headline">
-          Feels{this.state.feels ? this.state.feels + 'Man' : ''}
-        </div>
-        {this.state.feels !== 'foo'
-          ? <Checkbox
-              id="toggle-field"
-              label="Toggle field"
-              checked={this.state.showField}
-              onChange={this.toggleField}
-            />
-          : null}
-        <br />
-        {this.state.showField
-          ? <Textfield
-              label="Tell us how you feel"
-              value={this.state.feels}
-              onChange={this.onChangeFeels}
-            />
-          : null}
       </div>
     );
   }
--- src/components/__tests__/Drawer.spec.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Drawer from '../Drawer';
-
-it('renders without crashing', () => {
-  shallow(<Drawer open={false} />);
-});
